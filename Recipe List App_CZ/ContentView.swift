323dfbb776bd5b80e795c48f8efd254468d5bb7e
Christian Zagorski <christianz@hey.com>
2021-03-06 20:19:08 -0800

    Initial Commit

---
+++ Recipe List App_CZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe List App_CZ
+//
+//  Created by Christian Zagorski on 3/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe List App_CZ/Recipe_List_App_CZApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_App_CZApp.swift
+//  Recipe List App_CZ
+//
+//  Created by Christian Zagorski on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_App_CZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
